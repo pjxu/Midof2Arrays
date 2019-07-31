e4e49667edc31a0a26152ce9c9816e9409e8cef7
Jinx <19243068+pjxu@users.noreply.github.com>
2019-07-31 12:19:47 +0800

    Add files via upload

--- ArraysMedian/src/ArrMed.cpp
+++ ArraysMedian/src/ArrMed.cpp
@@ -1,11 +1,18 @@
-//Find the Median of Two Arrays.cpp
-// 4.12.2019
+/**
+* \brief Test Finding the Median of Two Arrays
+* \file   ArrMed.cpp
+*
+* history records:
+* \date 7-31-2019   modify the file
+*
+*/
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <iomanip>
 using namespace std;
-class MidoArrays {
+class MidoArrays
+{
 public:
 	void getInput();
 	void getLen();
@@ -23,14 +30,19 @@ private:
 	int item;
 };
 
-void MidoArrays::getInput() {
+/* \brief 
+* Get User Input
+*/
+void MidoArrays::getInput() 
+{
 	cout << "Input two arrays: " << endl;
 	cout << "How many items for the 1st one? ";
 	cin >> len1;
 	cout << "Now input individual items: ";
 	cin >> item;
 	arr1.push_back(item);
-	for (int m = 0; m < (len1 - 1); ++m) {
+	for (int m = 0; m < (len1 - 1); ++m) 
+    {
 		cout << "Now input individual items: ";
 		cin >> item;
 		arr1.push_back(item);
@@ -40,7 +52,8 @@ void MidoArrays::getInput() {
 	cout << "Now input individual items: ";
 	cin >> item;
 	arr2.push_back(item);
-	for (int n = 0; n < (len2 - 1); ++n) {
+	for (int n = 0; n < (len2 - 1); ++n) 
+    {
 		cout << "Now input individual items: ";
 		cin >> item;
 		arr2.push_back(item);
@@ -49,37 +62,58 @@ void MidoArrays::getInput() {
 	cout << "Array One has " << arr1.size() << " items" << endl << "Array Two has " << arr2.size() << " items" << endl;
 }
 
-void MidoArrays::getLen() {
+/* \brief
+* Get Array Lengths
+*/
+void MidoArrays::getLen() 
+{
 	vecLen = len1 + len2;
 }
 
-void MidoArrays::findMid() {
-	for (int m = 0; m < len1; ++m) {
+/* \brief: major Algorithm Function
+* Find Array Medians
+*/
+void MidoArrays::findMid() 
+{
+	for (int m = 0; m < len1; ++m) 
+    {
 		ints.push_back(arr1[m]);
 	}
-	for (int n = 0; n < len2; ++n) {
+	for (int n = 0; n < len2; ++n) 
+    {
 		ints.push_back(arr2[n]);
 	}
 	sort(ints.begin(), ints.end());
-	if ((vecLen % 2) != 0) {
+	if ((vecLen % 2) != 0) 
+    {
 		median = ints[(vecLen-1)/2];
 	}
-	else {
+	else 
+    {
 		median = (double(ints[(vecLen/2)] + ints[(vecLen/2)-1]))/2.0;
 	}
 }
 
-void MidoArrays::getOutput() {
+/* \brief: Output Function
+* Print to Console
+*/
+void MidoArrays::getOutput() 
+{
 	cout << "The Median is " << setprecision(3) << median << endl;
 }
 
-void MidoArrays::clearVec() {
+void MidoArrays::clearVec() 
+{
 	arr1.clear();
 	arr2.clear();
 	ints.clear();
 }
 
-int main() {
+/* \brief: RUN
+* main() function
+*/
+int main() 
+{
 	MidoArrays m;
 	m.getInput();
 	m.getLen();
@@ -99,7 +133,8 @@ int main() {
 		char c;
 		cout << "Continue?(y/n)";
 		cin >> c;
-		if (c != 'y') {
+		if (c != 'y') 
+        {
 			break;
 		}
 	}
